8c8f7f495c957c1508707c810d5c70b2baf79cc0
qiuxs1162 <1162796928@qq.com>
2017-12-21 18:33:03 +0800

    完善淘宝配置控制类 配置淘宝配置

--- code/src/main/java/com/qiuxs/uzhe/alimama/TaoBaoConfig.java
+++ code/src/main/java/com/qiuxs/uzhe/alimama/TaoBaoConfig.java
@@ -1,7 +1,76 @@
 package com.qiuxs.uzhe.alimama;
 
+import com.qiuxs.fdn.uconfig.IConfiguration;
+import com.qiuxs.fdn.uconfig.UConfigUtils;
+
+/**
+ * 淘宝配置管理
+ * @author qiuxs
+ *
+ */
 public class TaoBaoConfig {
-	
-	
-	
+
+	/*
+	 * 配置信息来自于 淘宝开放平台
+	 * http://open.taobao.com/
+	 */
+
+	/** 配置域 */
+	private static final String CONFIG_DOMAIN = "taobao";
+	/** ApiUrl配置Key */
+	private static final String URL = "url";
+	/** AppKey配置Key */
+	private static final String APP_KEY = "AppKey";
+	/** AppSecret配置Key */
+	private static final String APP_SECRET = "AppSecret";
+
+	/** ApiUrl */
+	private String url;
+	/** AppKey */
+	private String app_key;
+	/** AppSecret */
+	private String app_secret;
+
+	private static class InstanceHolder {
+		private static final TaoBaoConfig instance = new TaoBaoConfig();
+	}
+
+	private TaoBaoConfig() {
+		IConfiguration config = UConfigUtils.getConfig(CONFIG_DOMAIN);
+		this.url = config.getString(URL);
+		this.app_key = config.getString(APP_KEY);
+		this.app_secret = config.getString(APP_SECRET);
+	}
+
+	/**
+	 * 获取实例
+	 * @return
+	 */
+	public static TaoBaoConfig getInstance() {
+		return InstanceHolder.instance;
+	}
+
+	/**
+	 * 获取ApiUrl
+	 * @return
+	 */
+	public String getUrl() {
+		return this.url;
+	}
+
+	/**
+	 * 获取AppKey
+	 * @return
+	 */
+	public String getAppKey() {
+		return this.app_key;
+	}
+
+	/**
+	 * 获取AppSecret
+	 * @return
+	 */
+	public String getAppSecret() {
+		return this.app_secret;
+	}
 }
